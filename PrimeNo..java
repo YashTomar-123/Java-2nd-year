e7162490f7ef3bc5c11ded683130cf54630a95a1
YashTomar-123 <yash.tomar_cs24@gla.ac.in>
2025-08-02 18:57:51 +0530

    Create PrimeNo..java

---
+++ PrimeNo..java
@@ -0,0 +1,24 @@
+package Fundamentals;
+
+import java.util.Scanner;
+//Prime no.
+public class PrimeNo {
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		int n  = Sc.nextInt();
+		int i = 1;
+		int counter = 0;
+		while(i<=n) {
+			if(n%i==0) {
+				counter++;
+			}
+			i++;
+		}
+		if(counter>2) {
+			System.out.println("Not a Prime No.");
+		}
+		else {
+			System.out.println("Prime No.");
+		}
+	}
+}
